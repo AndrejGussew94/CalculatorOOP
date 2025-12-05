7254f8ee1227d891474467229e73b64bffc6263d
AndrejGussew94 <andrejgussew94@gmail.com>
2025-12-05 19:29:50 +0300

    ImGui basic calculator add

---
+++ build/CMakeFiles/Calculator.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Calculator.dir/imgui/backends/imgui_impl_glfw.cpp.o"
+  "CMakeFiles/Calculator.dir/imgui/backends/imgui_impl_glfw.cpp.o.d"
+  "CMakeFiles/Calculator.dir/imgui/backends/imgui_impl_opengl3.cpp.o"
+  "CMakeFiles/Calculator.dir/imgui/backends/imgui_impl_opengl3.cpp.o.d"
+  "CMakeFiles/Calculator.dir/imgui/imgui.cpp.o"
+  "CMakeFiles/Calculator.dir/imgui/imgui.cpp.o.d"
+  "CMakeFiles/Calculator.dir/imgui/imgui_demo.cpp.o"
+  "CMakeFiles/Calculator.dir/imgui/imgui_demo.cpp.o.d"
+  "CMakeFiles/Calculator.dir/imgui/imgui_draw.cpp.o"
+  "CMakeFiles/Calculator.dir/imgui/imgui_draw.cpp.o.d"
+  "CMakeFiles/Calculator.dir/imgui/imgui_tables.cpp.o"
+  "CMakeFiles/Calculator.dir/imgui/imgui_tables.cpp.o.d"
+  "CMakeFiles/Calculator.dir/imgui/imgui_widgets.cpp.o"
+  "CMakeFiles/Calculator.dir/imgui/imgui_widgets.cpp.o.d"
+  "CMakeFiles/Calculator.dir/main.cpp.o"
+  "CMakeFiles/Calculator.dir/main.cpp.o.d"
+  "Calculator"
+  "Calculator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Calculator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
